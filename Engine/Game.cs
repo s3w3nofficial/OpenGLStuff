1bdf59f00b488dbcd3af589874a6db811a8dc841
Tomas Pelak <info@pelaktomas.cz>
2019-10-12 14:40:28 -1000

    fix some stuff

--- Engine/Game.cs
+++ Engine/Game.cs
@@ -16,25 +16,83 @@ namespace MainApp.Engine
     class Game
     {
         float[] vertices = {
-                -0.5f, 0.5f, 0f,//v0
-				-0.5f, -0.5f, 0f,//v1
-				0.5f, -0.5f, 0f,//v2
-				0.5f, 0.5f, 0f,//v3
-		};
+                -0.5f,0.5f,-0.5f,
+                -0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,0.5f,-0.5f,
 
-        int[] indices = {
-                0,1,3,//top left triangle (v0, v1, v3)
-				3,1,2//bottom right triangle (v3, v1, v2)
-		};
+                -0.5f,0.5f,0.5f,
+                -0.5f,-0.5f,0.5f,
+                0.5f,-0.5f,0.5f,
+                0.5f,0.5f,0.5f,
 
-        float[] textureCoords =
-        {
-            0f,0f,
-            0f,1f,
-            1f,1f,
-            1f,0f
+                0.5f,0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,0.5f,
+                0.5f,0.5f,0.5f,
+
+                -0.5f,0.5f,-0.5f,
+                -0.5f,-0.5f,-0.5f,
+                -0.5f,-0.5f,0.5f,
+                -0.5f,0.5f,0.5f,
+
+                -0.5f,0.5f,0.5f,
+                -0.5f,0.5f,-0.5f,
+                0.5f,0.5f,-0.5f,
+                0.5f,0.5f,0.5f,
+
+                -0.5f,-0.5f,0.5f,
+                -0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,0.5f
+
         };
 
+        float[] textureCoords = {
+
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0
+
+
+        };
+
+        int[] indices = {
+                0,1,3,
+                3,1,2,
+                4,5,7,
+                7,5,6,
+                8,9,11,
+                11,9,10,
+                12,13,15,
+                15,13,14,
+                16,17,19,
+                19,17,18,
+                20,21,23,
+                23,21,22
+
+        };
 
         public DisplayManager Display { get; private set; }
         public Loader Loader { get; private set; }
@@ -56,7 +114,7 @@ namespace MainApp.Engine
             this.TestModel = this.Loader.LoadToVAO(vertices, textureCoords, indices);
             this.texture = new ModelTexture(this.Loader.LoadTexture("texture"));
             this.texturedModel = new TexturedModel(this.TestModel, this.texture);
-            this.entity = new Entity(this.texturedModel, new Vector3(0, 0, -2f), 0, 0, 0, 1);
+            this.entity = new Entity(this.texturedModel, new Vector3(0, 0, -2f), 0, 0, 40, 1);
             this.camera = new Camera();
 
             this.Init();
--- Shaders/vertexShader.glsl
+++ Shaders/vertexShader.glsl
@@ -13,6 +13,8 @@ void main(void) {
 	
 	//gl_Position = vec4(position.x, position.y, position.z, 1.0);
 	//gl_Position = vec4(position.xyz, 1.0);
-	gl_Position = viewMatrix * transformationMatrix * projectionMatrix * vec4(position, 1.0);
+	vec4 word_position = viewMatrix * transformationMatrix * vec4(position, 1.0);
+	gl_Position = projectionMatrix * word_position;
+	//gl_Position = vec4(position, 1.0);
 	pass_textureCoords = textureCoords;
 }
