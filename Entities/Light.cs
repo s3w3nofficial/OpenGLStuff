6bc0923ce8bdeff8f2b0ffc2ea157a611729ee7f
Tomas Pelak <info@pelaktomas.cz>
2019-10-14 10:40:54 -1000

    add some models and lightclass

---
+++ Entities/Light.cs
@@ -0,0 +1,18 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainApp.Entities
+{
+    class Light
+    {
+        public Vector3 Position { get; private set; }
+        public Vector3 Colour { get; set; }
+        public Light(Vector3 position, Vector3 colour)
+        {
+            this.Position = position;
+            this.Colour = colour;
+        }
+    }
+}
